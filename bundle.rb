acbd6cf2777ece933af297b13dd473f9f08b84a6
sgtcoolguy <cwilliams@aptana.com>
2010-02-16 15:16:35 -0500

    Add explicit foldings at discrete levels via commands as per Andrew's request

--- bundle.rb
+++ bundle.rb
@@ -17,6 +17,10 @@ END
       folding_menu.command 'Collapse'
       folding_menu.command 'Expand All'
       folding_menu.command 'Collapse All'
+      folding_menu.menu 'Toggle Foldings at Level' do |toggle_level|
+        toggle_level.command 'All Levels'
+        1.upto(9).each {|i| toggle_level.command i.to_s }
+      end
     end    
     main_menu.menu 'Comments' do |submenu|
       submenu.command 'Comment Line / Selection'
--- commands/folding.rb
+++ commands/folding.rb
@@ -1,5 +1,6 @@
 require 'ruble'
 
+# Generic folding, explciitly expand or collapse based on current position
 with_defaults :input => :none, :output => :discard, :key_binding => "F1" do
   command 'Expand' do |cmd|
     cmd.invoke do |context|
@@ -24,4 +25,47 @@ with_defaults :input => :none, :output => :discard, :key_binding => "F1" do
       context.editor.editor_part.source_viewer.doOperation(org.eclipse.jface.text.source.projection.ProjectionViewer::COLLAPSE_ALL)
     end
   end
+end
+
+# Toggles folding at a specific level of nesting
+def toggle_folding(editor, level)
+  source_viewer = editor.editor_part.source_viewer
+  model = source_viewer.projection_annotation_model
+  annotations = []
+  positions = []
+  iter = model.annotation_iterator
+  while iter.hasNext()
+    a = iter.next
+    annotations << a
+    positions << model.getPosition(a)
+  end  
+  
+  to_toggle = []
+  # iterate over positions
+  # check if position is included by x other positions?
+  annotations.each_with_index do |a, index|
+    count = 0
+    position = positions[index]
+    positions.each {|p| count += 1 if p != position and p.includes(position.offset) }
+    to_toggle << a if count == level - 1
+  end
+
+  to_toggle.each {|t| model.toggleExpansionState(t) }
+end
+
+# Toggle Folding levels explicitly
+with_defaults :input => :none, :output => :discard, :key_binding => "OPTION+COMMAND+0" do
+  command 'All Levels' do |cmd|
+    cmd.invoke do |context|
+      # Toggle all levels
+      context.editor.editor_part.source_viewer.doOperation(org.eclipse.jface.text.source.projection.ProjectionViewer::COLLAPSE_ALL)
+    end
+  end  
+  
+  1.upto(9).each do |level|
+    command level.to_s do |cmd|
+      cmd.key_binding = "OPTION+COMMAND+" + level.to_s
+      cmd.invoke {|context| toggle_folding(context.editor, level) }
+    end
+  end  
 end
